e318e544ff119269376abea6a18d275ad56808d2
Susovan Dev <susovandas.dev@gmail.com>
2025-11-17 21:20:54 +0530

    Assignment(2):Create a dummmy DB for products

---
+++ app/dummyDB/product.dummy.ts
@@ -0,0 +1,23 @@
+interface IProductDocument {
+	_id: number;
+	name: string;
+	description: string;
+	price: number;
+	category: string;
+	inStock: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export const dummyProducts: IProductDocument[] = [
+	{
+		_id: 1,
+		name: 'Product 1',
+		description: 'Description 1',
+		price: 10,
+		category: 'Category 1',
+		inStock: true,
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	},
+];
