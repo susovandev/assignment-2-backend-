1c83420f1fe2d1b46485d713673eed69d091b39a
Susovan Dev <susovandas.dev@gmail.com>
2025-11-17 21:12:53 +0530

    Assignment(2): add custom logger instead of console

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,10 @@
 import app from './app';
 import envConfig from './config/env.config';
-
+import Logger from './utils/logger.utils';
 
 const env = envConfig.SERVER.NODE_ENV;
 const port = envConfig.SERVER.PORT;
 
 app.listen(port, () => {
-	console.log(`Server running in ${env} mode on  http://localhost:${port}`);
+	Logger.info(`Server running in ${env} mode on  http://localhost:${port}`);
 });
